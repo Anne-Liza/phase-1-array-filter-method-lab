c4b08acaadb56528cc965f22c929d189f4372808
Anne <annewangui210@gmail.com>
2024-07-11 19:15:36 +0300

    phase-1-array-filter-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase()=== name.toLowerCase());
+};
+console.log (findMatching(drivers,"Bobby"));
+console.log (findMatching(drivers,"anne"));
+console.log (findMatching(drivers,"sally"));
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase().startsWith(name.toLowerCase()));
+}
+console.log (fuzzyMatch(drivers,"bobby"));
+
+function matchName(drivers, name){
+return drivers.filter(driver => driver.name === name);
+};
+console.log(matchName(drivers,"Annette"));
+
+
